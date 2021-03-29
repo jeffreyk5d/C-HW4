308c870e02abeb309280758d0296510eb3ed0089
jeffreyk5d <63979061+jeffreyk5d@users.noreply.github.com>
2021-03-29 16:17:04 +0800

    Update 作業4.cpp

--- 作業4.cpp
+++ 作業4.cpp
@@ -1,6 +1,3 @@
-﻿// HW4.cpp : 定義主控台應用程式的進入點。
-//
-
 //#include "stdafx.h"
 #include <iostream>
 using namespace std;
@@ -42,7 +39,7 @@ void clear(linkedPolynomialTerm*& polynomialTermPtr)
 void inputTerms(polynomialTerm terms[], int coef, int expo)
 {
 	// add your code here
-	int input_posi=0;	
+	int input_posi = 0;
 	if (coef == 0) {												//係數為0 則不存入 直接返回
 		return;
 	}
@@ -55,13 +52,13 @@ void inputTerms(polynomialTerm terms[], int coef, int expo)
 		while (expo < terms[input_posi].expo) {
 			input_posi++;
 		}
-		if (expo==terms[input_posi].expo) {							//若輸入的次方已存在 則直接覆蓋他的coef 並返回
+		if (expo == terms[input_posi].expo) {							//若輸入的次方已存在 則直接覆蓋他的coef 並返回
 			terms[input_posi].coef = coef;
 			return;
 		}
-		for (int i = MAX_TERMS-2; i >= input_posi; i--) {			//往後移 PS.為避免資料被覆蓋，從最後面往後移 (terms[MAX_TERMS-2]移到terms[MAX_TERMS-1]，terms[MAX_TERMS-3]移到terms[MAX_TERMS-2].....)
+		for (int i = MAX_TERMS - 2; i >= input_posi; i--) {			//往後移 PS.為避免資料被覆蓋，從最後面往後移 (terms[MAX_TERMS-2]移到terms[MAX_TERMS-1]，terms[MAX_TERMS-3]移到terms[MAX_TERMS-2].....)
 			terms[i + 1] = terms[i];
-		}		
+		}
 		terms[input_posi].coef = coef;								//移動完後 terms[input_posi]裡的expo和coef應沒有值 直接存入即可
 		terms[input_posi].expo = expo;
 	}
@@ -77,55 +74,62 @@ void inputLinkTerms(linkedPolynomialTerm*& polyPtr, int coef, int expo)
 	tmpPtr->expo = expo;
 	// add your code here 
 	linkedPolynomialTerm* pre_posi;
-	linkedPolynomialTerm* cur_posi;	
-	pre_posi =polyPtr;
-	cur_posi =polyPtr;
+	linkedPolynomialTerm* cur_posi;
+	*&pre_posi = *&polyPtr;
+	*&cur_posi = *&polyPtr;
 	tmpPtr->nextTermPtr = nullptr;								//polyPtr->nextTermPtr原本沒指向任何東西(也沒指向nullptr) 所以無從判斷， 所一要在輸入第一個Link後 讓polyPtr->nextTermPtr指項nullptr;
-	if (coef == 0) {
+	if (tmpPtr->coef == 0) {
 		return;
 	}
 	if (!polyPtr) {													//若polyPtr裡沒有值 則將已存好input值的tmpPtr存入polyPtr的第一個Link
-		polyPtr = tmpPtr;		
+		polyPtr = tmpPtr;
 		return;
 	}
 	else {
 		while (cur_posi) {											//若cur_posi有值則繼續，若沒break;最後會停在沒有值的地方			
-			if (expo >= cur_posi->expo) {							//若expo大於cur_posi的expo 就break讓cur_posi留在它之後要放入的位置
+			if (tmpPtr->expo >= cur_posi->expo) {							//若expo大於cur_posi的expo 就break讓cur_posi留在它之後要放入的位置
 				break;
 			}
-			cur_posi= cur_posi->nextTermPtr;						
+			cur_posi = cur_posi->nextTermPtr;
 		}
-
 		while (pre_posi) {											//找cur_posi的前一項pre_posi
-			if (pre_posi->nextTermPtr==cur_posi) {
-				break;       
+			if (pre_posi->nextTermPtr == cur_posi) {
+				break;
 			}
 			pre_posi = pre_posi->nextTermPtr;
 		}
 
-		if (!cur_posi) {
+
+		if (!cur_posi) {											//輸入為最小次方則會跑到nullptr直接放入回傳即可
 			cur_posi = tmpPtr;
 			pre_posi->nextTermPtr = tmpPtr;
 			return;
 		}
-		if (expo == cur_posi->expo) {
-			if(cur_posi==polyPtr){
-                           polyPtr=tmpPtr；
-                           return; 
-                        }
-                        cur_posi = tmpPtr;
+		
+		
+		if (tmpPtr->expo == cur_posi->expo) {						//輸入次方已存在 則以新的係數coef取代 P.S.不知為何不能直接打cur_posi=tmpPtr;所以我們用跳過的方式取代
+			if (cur_posi == polyPtr) {				
+				tmpPtr->nextTermPtr = polyPtr->nextTermPtr;
+				polyPtr = tmpPtr;
+				return;
+			}
+			else {
+				pre_posi->nextTermPtr = tmpPtr;	
+				tmpPtr->nextTermPtr = cur_posi->nextTermPtr;
+			}
+			
 			return;
 		}
 
 		if (cur_posi == polyPtr) {									//若輸入為最大次方，則將tmpPtr下一項指向cur_posi，P.S.不能直接指向polyPtr，因為polyPtr一定要是Linklist的頭
-                        tmpPtr->nextTermPtr = cur_posi;
+			tmpPtr->nextTermPtr = cur_posi;
 			polyPtr = tmpPtr;										//再將tmpPtr的位置改成首項
 			return;													//直接回傳
 		}
 
 		pre_posi->nextTermPtr = tmpPtr;
 		tmpPtr->nextTermPtr = cur_posi;
-	}																																																				
+	}
 	return;
 }
 
@@ -136,7 +140,7 @@ void addArrayBasedPoly(polynomialTerm a[], polynomialTerm b[], polynomialTerm d[
 	int a_posi = 0;
 	int b_posi = 0;
 	int d_posi = 0;
-	while (a[a_posi].coef!=0||a[a_posi].expo!=0||b[b_posi].coef!=0||b[b_posi].expo!=0) { //當a[a_posi].coef沒有值&&a[a_posi].expo沒有值&&b[b_posi].coef沒有值&&b[b_posi].expo沒有值 時停下，Demorgan後: 當a[a_posi].coef有值||a[a_posi].expo有值||b[b_posi].coef有值||b[b_posi].expo有值 時執行
+	while (a[a_posi].coef != 0 || a[a_posi].expo != 0 || b[b_posi].coef != 0 || b[b_posi].expo != 0) { //當a[a_posi].coef沒有值&&a[a_posi].expo沒有值&&b[b_posi].coef沒有值&&b[b_posi].expo沒有值 時停下，Demorgan後: 當a[a_posi].coef有值||a[a_posi].expo有值||b[b_posi].coef有值||b[b_posi].expo有值 時執行
 		if (a[a_posi].expo > b[b_posi].expo) {											//當a[a_posi]的expo大於b[b_posi]的 將a的coef和expo存入d
 			d[d_posi].coef = a[a_posi].coef;
 			d[d_posi].expo = a[a_posi].expo;
@@ -149,8 +153,8 @@ void addArrayBasedPoly(polynomialTerm a[], polynomialTerm b[], polynomialTerm d[
 			d_posi++;
 			b_posi++;
 		}
-		else if(a[a_posi].expo == b[b_posi].expo){										//若等於則將a b的coef相加
-			if (a[a_posi].coef + b[b_posi].coef!=0) {									//但若相加後等於零則不存入
+		else if (a[a_posi].expo == b[b_posi].expo) {										//若等於則將a b的coef相加
+			if (a[a_posi].coef + b[b_posi].coef != 0) {									//但若相加後等於零則不存入
 				d[d_posi].coef = a[a_posi].coef + b[b_posi].coef;
 				d[d_posi].expo = a[a_posi].expo;
 				a_posi++;
@@ -162,25 +166,25 @@ void addArrayBasedPoly(polynomialTerm a[], polynomialTerm b[], polynomialTerm d[
 				b_posi++;
 			}
 		}
-	        if(a[a_posi].coef == 0 && a[a_posi].expo == 0&&d_posi!=MAX_TERMS){
-                  while(b[b_posi].coef != 0 && b[b_posi].expo != 0 && d_posi != MAX_TERMS){
-                     d[d_posi].coef = b[b_posi].coef;
-	             d[d_posi].expo = b[b_posi].expo;
-		     d_posi++;
-	             b_posi++;
-                  }
-                }
-                if(b[b_posi].coef == 0 && b[b_posi].expo == 0 && d_posi != MAX_TERMS){
-                  while (a[a_posi].coef != 0 && a[a_posi].expo != 0 && d_posi != MAX_TERMS){
-                    	d[d_posi].coef = a[a_posi].coef;
-			d[d_posi].expo = a[a_posi].expo;
-			d_posi++;
-			a_posi++;
-                  }
-                 }
-               
+		if (a[a_posi].coef == 0 && a[a_posi].expo == 0 && d_posi != MAX_TERMS) {
+			while (b[b_posi].coef != 0 && b[b_posi].expo != 0 && d_posi != MAX_TERMS) {
+				d[d_posi].coef = b[b_posi].coef;
+				d[d_posi].expo = b[b_posi].expo;
+				d_posi++;
+				b_posi++;
+			}
+		}
+		if (b[b_posi].coef == 0 && b[b_posi].expo == 0 && d_posi != MAX_TERMS) {
+			while (a[a_posi].coef != 0 && a[a_posi].expo != 0 && d_posi != MAX_TERMS) {
+				d[d_posi].coef = a[a_posi].coef;
+				d[d_posi].expo = a[a_posi].expo;
+				d_posi++;
+				a_posi++;
+			}
+		}
+
 	}
-	
+
 
 	return;
 }
@@ -189,7 +193,57 @@ linkedPolynomialTerm* addLinkBasedPoly(linkedPolynomialTerm* aPtr, linkedPolynom
 {
 	linkedPolynomialTerm* dPtr = nullptr;
 	// add your code here
-
+	
+	linkedPolynomialTerm* a_posi;
+	linkedPolynomialTerm* b_posi;
+	linkedPolynomialTerm* d_posi;
+	linkedPolynomialTerm* tmpPtr;
+	tmpPtr = new linkedPolynomialTerm;
+	a_posi = aPtr;
+	b_posi = bPtr;
+	d_posi = dPtr;
+	tmpPtr->nextTermPtr = nullptr;	
+	while (a_posi || b_posi) {
+		if ((a_posi->expo) > (b_posi->expo)) {			
+			tmpPtr= a_posi;			
+			a_posi = a_posi->nextTermPtr;
+			d_posi = tmpPtr;
+			d_posi=d_posi->nextTermPtr;
+		}else if (b_posi->expo > a_posi->expo) {
+			tmpPtr = b_posi;
+			d_posi = tmpPtr;
+			b_posi = b_posi->nextTermPtr;
+			d_posi = d_posi->nextTermPtr;
+		}else if ((a_posi->expo) == (b_posi->expo)) {
+			if ((a_posi->coef )+ (b_posi->coef) != 0) {
+				tmpPtr->coef = (a_posi->coef) + (b_posi->coef);
+				tmpPtr->expo = a_posi->expo;
+				d_posi = tmpPtr;
+				a_posi = a_posi->nextTermPtr;
+				b_posi = b_posi->nextTermPtr;
+				d_posi = d_posi->nextTermPtr;
+			}
+			else {
+				a_posi = a_posi->nextTermPtr;
+				b_posi = b_posi->nextTermPtr;
+			}
+			
+		}else if (!b_posi) {
+			while (a_posi) {
+				tmpPtr = a_posi;
+				a_posi = a_posi->nextTermPtr;
+				d_posi = tmpPtr;
+				d_posi = d_posi->nextTermPtr;
+			}
+		}else if (!a_posi) {
+			while (b_posi) {
+				tmpPtr = b_posi;
+				b_posi = b_posi->nextTermPtr;
+				d_posi = tmpPtr;
+				d_posi = d_posi->nextTermPtr;
+			}
+		}
+	}
 
 
 	return dPtr;
